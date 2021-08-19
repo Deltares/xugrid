b71b3a89ab01c265133649680acbc773f44928bb
Huite Bootsma <huitebootsma@gmail.com>
2021-08-19 18:15:46 +0200

    Add to_snap method. Also everything should be vectorized now.

---
+++ tests/test_snap.py
@@ -0,0 +1,89 @@
+import numpy as np
+import pytest
+
+from xugrid.snapping import snap, snap_to
+
+
+def test_snap__three_points():
+    x = y = np.array([0.0, 1.0, 1.5])
+    inv_perm, snap_x, snap_y = snap(x, y, 0.1)
+    assert inv_perm is None
+    assert np.array_equal(x, snap_x)
+    assert np.array_equal(y, snap_y)
+
+    # hypot(0.5, 0.5) = 0.707...
+    inv_perm, snap_x, snap_y = snap(x, y, 0.71)
+    expected_inv_perm = np.array([0, 1, 1])
+    expected_x = expected_y = np.array([0.0, 1.25])
+    assert np.array_equal(inv_perm, expected_inv_perm)
+    assert np.array_equal(snap_x, expected_x)
+    assert np.array_equal(snap_y, expected_y)
+
+    # hypot(1, 1) = 1.414...
+    inv_perm, snap_x, snap_y = snap(x, y, 1.42)
+    expected_inv_perm = np.array([0, 0, 0])
+    assert np.array_equal(inv_perm, expected_inv_perm)
+    assert np.allclose(snap_x, np.array([2.5 / 3]))
+    assert np.allclose(snap_y, np.array([2.5 / 3]))
+
+
+def test_snap__two_lines():
+    x = np.array([0.0, 1.0, 1.02, 2.0])
+    y = np.array([1.0, 0.0, 0.0, 1.0])
+    edge_node_connectivity = np.array(
+        [
+            [0, 1],
+            [2, 3],
+        ]
+    )
+    inv_perm, snap_x, snap_y = snap(x, y, 0.1)
+    c = inv_perm[edge_node_connectivity]
+
+    expected_inv_perm = np.array([0, 1, 1, 2])
+    expected_x = np.array([0.0, 1.01, 2.0])
+    expected_y = np.array([1.0, 0.0, 1.0])
+    expected_c = np.array(
+        [
+            [0, 1],
+            [1, 2],
+        ]
+    )
+    assert np.array_equal(inv_perm, expected_inv_perm)
+    assert np.array_equal(snap_x, expected_x)
+    assert np.array_equal(snap_y, expected_y)
+    assert np.array_equal(c, expected_c)
+
+
+def test_snap_to():
+    x = np.array([1.0, 2.0, 3.0])
+    y = np.array([1.0, 2.0, 3.0])
+    to_x = x + 0.1
+    to_y = y + 0.1
+
+    # None snapped
+    snap_x, snap_y = snap_to(x, y, to_x, to_y, 0.1)
+    assert np.array_equal(snap_x, x)
+    assert np.array_equal(snap_y, y)
+
+    # All snapped
+    snap_x, snap_y = snap_to(x, y, to_x, to_y, 0.2)
+    assert np.array_equal(snap_x, to_x)
+    assert np.array_equal(snap_y, to_y)
+
+    # Ties, no tiebreaker
+    with pytest.raises(ValueError):
+        snap_x, snap_y = snap_to(x, y, to_x, to_y, 3.0)
+
+    # Take nearest
+    snap_x, snap_y = snap_to(x, y, to_x, to_y, 3.0, tiebreaker="nearest")
+    assert np.array_equal(snap_x, to_x)
+    assert np.array_equal(snap_y, to_y)
+
+    # More ties
+    to_x = np.array([1.01, 2.01, 2.002, 3.01])
+    to_y = np.array([1.01, 2.01, 2.002, 3.01])
+    snap_x, snap_y = snap_to(x, y, to_x, to_y, 0.5, tiebreaker="nearest")
+    expected_x = np.array([1.01, 2.002, 3.01])
+    expected_y = np.array([1.01, 2.002, 3.01])
+    assert np.array_equal(snap_x, expected_x)
+    assert np.array_equal(snap_y, expected_y)
--- xugrid/snapping.py
+++ xugrid/snapping.py
@@ -5,35 +5,70 @@ from typing import Tuple
 
 import numpy as np
 import pandas as pd
+from numpy.lib.shape_base import column_stack
 from scipy import sparse
 from scipy.sparse.csgraph import connected_components
-from scipy.spatial import KDTree
+from scipy.spatial import cKDTree
 
-FloatArray = np.ndarray
-IntArray = np.ndarray
+from .typing import FloatArray, IntArray
 
 
 def snap(
-    x: FloatArray, y: FloatArray, distance: float
+    x: FloatArray, y: FloatArray, max_distance: float
 ) -> Tuple[FloatArray, FloatArray, IntArray]:
-    # First, find all the points that lie within distance of each other
+    """
+    Snap neigbhoring vertices together that are located within a maximum
+    distance from each other.
+
+    If vertices are located within a maximum distance, they are merged into a
+    single vertex. The coordinates of the merged coordinates are given by the
+    centroid of the merging vertices.
+
+    Note that merging is a communicative process: vertex A might lie close to
+    vertex B, vertex B might lie close to vertex C, and so on. These points are
+    grouped and merged into a single new vertex.
+
+    Parameters
+    ----------
+    x: 1D nd array of floats of size N
+    y: 1D nd array of floats of size N
+    max_distance: float
+
+    Returns
+    -------
+    inv_perm: 1D nd array of ints of size N
+        Inverse permutation: the new vertex number for every old vertex.
+        Is None when no vertices within max_distanceof each other.
+    x_merged: 1D nd array of floats of size M
+        Returns x when no vertices within max_distanceof each other.
+    y_merged: 1D nd array of floats of size M
+        Returns y when no vertices within max_distanceof each other.
+    """
+    # First, find all the points that lie within max_distance of each other
     coords = np.column_stack((x, y))
     n = len(coords)
-    tree = KDTree(coords)
-    pairs = np.array([*tree.query_pairs(r=distance)])
+    tree = cKDTree(coords)
+    coo_distances = tree.sparse_distance_matrix(
+        tree, max_distance=max_distance, output_type="coo_matrix"
+    )
+    # Get rid of diagonal
+    i = coo_distances.row
+    j = coo_distances.col
+    off_diagonal = i != j
+    i = i[off_diagonal]
+    j = j[off_diagonal]
     # If no pairs are found, there is nothing to snap
-    if len(pairs) > 0:
+    if len(i) > 0:
         # Next, group the points together.
         # Point A might lie close to point B, point B might lie close to
         # Point C, and so on. These points are grouped, and their centroid
         # is computed.
-        i = pairs[:, 0]
-        j = pairs[:, 1]
         coo_content = (np.ones(i.size), (i, j))
         coo_matrix = sparse.coo_matrix(coo_content, shape=(n, n))
-        _, labels = connected_components(coo_matrix, directed=False)
+        # Directed is true: this matrix is symmetrical
+        _, inv_perm = connected_components(coo_matrix, directed=True)
         new = (
-            pd.DataFrame({"label": labels, "x": x, "y": y})
+            pd.DataFrame({"label": inv_perm, "x": x, "y": y})
             .groupby("label")
             .agg(
                 {
@@ -42,9 +77,85 @@ def snap(
                 }
             )
         )
-        # In case of a connectivity array, labels can be used to index into,
+        # In case of a connectivity array, inv_perm can be used to index into,
         # yielding the updated numbers. E.g.:
-        # updated_face_nodes = labels[face_nodes]
-        return labels, new["x"].values, new["y"].values
+        # updated_face_nodes = inv_perm[face_nodes]
+        return inv_perm, new["x"].values, new["y"].values
     else:
         return None, x, y
+
+
+def snap_to(
+    x: FloatArray,
+    y: FloatArray,
+    to_x: FloatArray,
+    to_y: FloatArray,
+    max_distance: float,
+    tiebreaker=None,
+) -> Tuple[FloatArray, FloatArray, IntArray]:
+    """
+    Snap vertices (x, y) to another set of vertices (to_x, to_y) if within a
+    specified maximum distance.
+
+    Parameters
+    ----------
+    x: 1D nd array of floats of size N
+    y: 1D nd array of floats of size N
+    to_x: 1D nd array of floats of size M
+    to_y: 1D nd array of floats of size M
+    max_distance: float
+
+    Returns
+    -------
+    x_snapped: 1D nd array of floats of size N
+    y_snapped: 1D nd array of floats of size N
+    """
+    if tiebreaker not in (None, "nearest"):
+        raise ValueError(
+            f"Invalid tiebreaker: {tiebreaker}, should be one of "
+            '{None, "nearest"} instead.'
+        )
+    # Build KDTrees
+    coords = np.column_stack((x, y))
+    to_coords = np.column_stack((to_x, to_y))
+    tree = cKDTree(coords)
+    to_tree = cKDTree(to_coords)
+
+    # Build distance matrix, identify points to snap (update) and possible ties
+    # row (i) is index in x, y; column (j) is index in to_x, to_y
+    # Convert to csr for quicker row indexing, and row reductions.
+    distances = tree.sparse_distance_matrix(
+        to_tree, max_distance=max_distance, output_type="coo_matrix"
+    ).tocsr()
+    n_per_row = distances.getnnz(axis=1)
+    update = n_per_row == 1
+    tie = n_per_row > 1
+
+    # Update the points that snap to a single (to_x, to_y) vertex
+    xnew = x.copy()
+    ynew = y.copy()
+    j_update = distances[update].indices
+    xnew[update] = to_x[j_update]
+    ynew[update] = to_y[j_update]
+
+    # Resolve ties with multiple (to_x, to_y) candidates
+    if tie.any():
+        if tiebreaker == "nearest":
+            ties = distances[tie].tocoo()
+            j_nearest = (
+                pd.DataFrame({"i": ties.row, "distance": ties.data}, index=ties.col)
+                .groupby("i")["distance"]
+                .idxmin()
+                .values
+            )
+            xnew[tie] = to_x[j_nearest]
+            ynew[tie] = to_y[j_nearest]
+        elif tiebreaker is None:
+            raise ValueError(
+                "Ties detected: multiple options to snap to, given max distance: "
+                "set a smaller tolerance or specify a tiebreaker."
+            )
+        else:
+            raise ValueError("Invalid tiebreaker")
+
+    return xnew, ynew
