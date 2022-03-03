9584f9cda526bdf81649a0bf56e62acc4809cbde
Huite Bootsma <huitebootsma@gmail.com>
2022-03-03 17:48:11 +0100

    Remove _templates; do more via CSS, html_theme_options; include name in logo

--- docs/conf.py
+++ docs/conf.py
@@ -97,17 +97,27 @@ pygments_style = "sphinx"
 #
 html_theme = "pydata_sphinx_theme"
 
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-#
-# html_theme_options = {}
-
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 html_css_files = ["theme-deltares.css"]
+
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+#
+html_theme_options = {
+    "navbar_align": "content",
+    "icon_links": [
+        {
+            "name": "Deltares",
+            "url": "https://deltares.nl/en/",
+            "icon": "_static/deltares-white.svg",
+            "type": "local",
+        },
+    ],
+}
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
@@ -121,8 +131,7 @@ html_css_files = ["theme-deltares.css"]
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = "_static/deltares-white.svg"
-xugrid_logo = "_static/xugrid-low-res.svg"
+html_logo = "_static/xugrid-low-res.svg"
 
 # -- Options for HTMLHelp output ---------------------------------------------
 
