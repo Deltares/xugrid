060d63a9ac4747eea3648fa01e5ae5c5966d3172
Huite Bootsma <huitebootsma@gmail.com>
2023-03-05 17:29:22 +0100

    Add COO weight matrix

--- tests/test_regrid/test_weight_matrix.py
+++ tests/test_regrid/test_weight_matrix.py
@@ -5,26 +5,61 @@ from xugrid.regrid import weight_matrix
 
 
 @pytest.fixture(scope="function")
-def matrix():
+def coo_matrix():
     source_index = np.arange(10)
     target_index = np.repeat(np.arange(5), 2)
     weights = np.full(10, 0.5)
-    return weight_matrix.create_weight_matrix(target_index, source_index, weights)
+    return weight_matrix.weight_matrix_coo(source_index, target_index, weights)
 
 
-def test_create_weight_matrix(matrix):
-    assert isinstance(matrix, weight_matrix.WeightMatrixCSR)
-    assert np.array_equal(matrix.indptr, [0, 2, 4, 6, 8, 10])
-    assert np.array_equal(matrix.indices, np.arange(10))
-    assert np.allclose(matrix.weights, np.full(10, 0.5))
-    assert matrix.n == 5
-    assert matrix.nnz == 10
+@pytest.fixture(scope="function")
+def csr_matrix():
+    source_index = np.arange(10)
+    target_index = np.repeat(np.arange(5), 2)
+    weights = np.full(10, 0.5)
+    return weight_matrix.weight_matrix_csr(source_index, target_index, weights)
 
 
-def test_nzrange(matrix):
-    i, w = weight_matrix.nzrange(matrix, 0)
+def test_weight_matrix_coo(coo_matrix):
+    assert isinstance(coo_matrix, weight_matrix.WeightMatrixCOO)
+    assert np.allclose(coo_matrix.data, np.full(10, 0.5))
+    assert np.array_equal(coo_matrix.row, [0, 0, 1, 1, 2, 2, 3, 3, 4, 4])
+    assert np.array_equal(coo_matrix.col, np.arange(10))
+    assert coo_matrix.nnz == 10
+
+
+def test_weight_matrix_csr(csr_matrix):
+    assert isinstance(csr_matrix, weight_matrix.WeightMatrixCSR)
+    assert np.allclose(csr_matrix.data, np.full(10, 0.5))
+    assert np.array_equal(csr_matrix.indices, np.arange(10))
+    assert np.array_equal(csr_matrix.indptr, [0, 2, 4, 6, 8, 10])
+    assert csr_matrix.n == 5
+    assert csr_matrix.nnz == 10
+
+
+def test_nzrange(csr_matrix):
+    i, w = weight_matrix.nzrange(csr_matrix, 0)
     assert np.array_equal(i, [0, 1])
     assert np.allclose(w, [0.5, 0.5])
-    i, w = weight_matrix.nzrange(matrix, 1)
+    i, w = weight_matrix.nzrange(csr_matrix, 1)
     assert np.array_equal(i, [2, 3])
     assert np.allclose(w, [0.5, 0.5])
+
+
+def test_coo_to_csr(coo_matrix):
+    csr_matrix = weight_matrix.coo_to_csr(coo_matrix)
+    assert isinstance(csr_matrix, weight_matrix.WeightMatrixCSR)
+    assert np.allclose(csr_matrix.data, np.full(10, 0.5))
+    assert np.array_equal(csr_matrix.indices, np.arange(10))
+    assert np.array_equal(csr_matrix.indptr, [0, 2, 4, 6, 8, 10])
+    assert csr_matrix.n == 5
+    assert csr_matrix.nnz == 10
+
+
+def test_csr_to_coo(csr_matrix):
+    coo_matrix = weight_matrix.csr_to_coo(csr_matrix)
+    assert isinstance(coo_matrix, weight_matrix.WeightMatrixCOO)
+    assert np.allclose(coo_matrix.data, np.full(10, 0.5))
+    assert np.array_equal(coo_matrix.row, [0, 0, 1, 1, 2, 2, 3, 3, 4, 4])
+    assert np.array_equal(coo_matrix.col, np.arange(10))
+    assert coo_matrix.nnz == 10
--- xugrid/regrid/weight_matrix.py
+++ xugrid/regrid/weight_matrix.py
@@ -2,6 +2,10 @@
 The weight matrix contains the target indices (rows), the source indices
 (columns), and the weights. These are stored in compressed sparse row format
 (CSR).
+
+We store the linear target index in the column: then all the source indices may
+be found together for a single target index. This is useful during querying,
+and during regridding: in both case, process on target cell at a time.
 """
 from typing import NamedTuple, Tuple
 
@@ -11,43 +15,119 @@ import numpy as np
 from xugrid.constants import FloatArray, IntArray, IntDType
 
 
-class WeightMatrixCSR(NamedTuple):
+class WeightMatrixCOO(NamedTuple):
     """
+    A sparse matrix in COOrdinate format, also known as the "ijv" or "triplet"
+    format. More or less matches the scipy.sparse.coo_matrix.
+
     NamedTuple for easy ingestion by numba.
 
-    More or less matches the scipy.sparse.csr_matrix.
+    Parameters
+    ----------
+    data: np.ndarray of floats
+        The regridding weights.
+    row: np.ndarray of integers
+        The linear index into the source grid.
+    col: np.ndarray of integers
+        The linear index into the target grid.
+    nnz: int
+        The number of non-zero values.
     """
 
-    indptr: IntArray
+    data: FloatArray
+    row: IntArray
+    col: IntArray
+    nnz: int
+
+
+class WeightMatrixCSR(NamedTuple):
+    """
+    Compressed Sparse Row matrix. The row indices are compressed; all values
+    must therefore be sorted by row number. More or less matches the
+    scipy.sparse.csr_matrix.
+
+    NamedTuple for easy ingestion by numba.
+
+    Parameters
+    ----------
+    data: np.ndarray of floats
+        The regridding weights.
+    indices: np.ndarray of integers
+        The column numbers of the CSR format. The linear index into the source
+        grid.
+    indptr: inp.ndarray of integers
+        The row index. Values for row i (target index i) are stored in:
+        indices[indptr[i]: indptr[i + 1]]
+    n: int
+        The number of rows.
+    nnz: int
+        The number of non-zero values.
+    """
+
+    data: FloatArray
     indices: IntArray
-    weights: FloatArray
+    indptr: IntArray
     n: int
     nnz: int
 
 
-def create_weight_matrix(
-    target_index: IntArray,
+@numba.njit(inline="always")
+def nzrange(A: WeightMatrixCSR, column: int) -> Tuple[IntArray, FloatArray]:
+    """
+    Return the indices and values of a single column
+    """
+    start = A.indptr[column]
+    end = A.indptr[column + 1]
+    return A.indices[start:end], A.data[start:end]
+
+
+def weight_matrix_coo(
     source_index: IntArray,
-    weights: FloatArray,
+    target_index: IntArray,
+    weight_values: FloatArray,
+) -> WeightMatrixCOO:
+    return WeightMatrixCOO(
+        weight_values,
+        target_index,
+        source_index,
+        weight_values.size,
+    )
+
+
+def weight_matrix_csr(
+    source_index: IntArray,
+    target_index: IntArray,
+    weight_values: FloatArray,
 ) -> WeightMatrixCSR:
-    i = np.cumsum(np.bincount(target_index))
+    coo = weight_matrix_coo(source_index, target_index, weight_values)
+    return coo_to_csr(coo)
+
+
+def coo_to_csr(matrix: WeightMatrixCOO) -> WeightMatrixCSR:
+    """
+    Convert COO matrix to CSR matrix.
+
+    Assumes the COO matrix indices are already sorted by row number!
+    """
+    i = np.cumsum(np.bincount(matrix.row))
     indptr = np.empty(i.size + 1, dtype=IntDType)
     indptr[0] = 0
     indptr[1:] = i
     return WeightMatrixCSR(
+        matrix.data,
+        matrix.col,
         indptr,
-        source_index,
-        weights,
         indptr.size - 1,
-        source_index.size,
+        matrix.data.size,
     )
 
 
-@numba.njit(inline="always")
-def nzrange(A: WeightMatrixCSR, row: int) -> Tuple[IntArray, FloatArray]:
+def csr_to_coo(matrix: WeightMatrixCSR) -> WeightMatrixCOO:
     """
-    Return the indices and values of a single row
+    Convert CSR matrix to COO matrix.
+
+    Expand the indtpr to full row numbers.
     """
-    start = A.indptr[row]
-    end = A.indptr[row + 1]
-    return A.indices[start:end], A.weights[start:end]
+    n_repeat = np.diff(matrix.indptr)
+    row = np.repeat(np.arange(matrix.n), n_repeat)
+    return WeightMatrixCOO(matrix.data, row, matrix.indices, matrix.nnz)
