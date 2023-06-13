5aa97faf4bb843ceb9e2e3c3538cc0fd554c2a19
Huite <Huite@users.noreply.github.com>
2023-06-13 13:50:34 +0000

    deploy: cc8690e71dc1c96dd2edfe13f51852476d6bd8a2

---
+++ _downloads/734d8ff5500153e9a43e65a9cf501507/elevation_nl.py
@@ -0,0 +1,21 @@
+"""
+Elevation NL
+============
+
+This is a small dataset containing a triangulation of a digital elevation model
+of the Netherlands.
+"""
+
+import matplotlib.pyplot as plt
+
+import xugrid
+
+uda = xugrid.data.elevation_nl()
+
+section_y = 475_000.0
+section = uda.ugrid.sel(y=section_y)
+
+fig, (ax0, ax1) = plt.subplots(figsize=(22.6, 10), ncols=2)
+uda.ugrid.plot(ax=ax0, vmin=-20, vmax=90, cmap="terrain")
+ax0.axhline(y=section_y, color="red")
+section.plot(ax=ax1, x="x")
