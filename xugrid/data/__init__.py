0c47734113309679ee1786c62cb06037bd42c69a
veenstrajelmer <veenstrajelmer@gmail.com>
2023-07-07 13:20:49 +0200

    added grevelingen example data

--- xugrid/data/__init__.py
+++ xugrid/data/__init__.py
@@ -1,2 +1,2 @@
-from xugrid.data.sample_data import adh_san_diego, elevation_nl, xoxo
+from xugrid.data.sample_data import adh_san_diego, elevation_nl, xoxo, grevelingen
 from xugrid.data.synthetic import disk, generate_disk
