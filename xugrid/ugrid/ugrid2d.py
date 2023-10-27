e378cf776b73c4caafaaa63d4068098ab813efba
veenstrajelmer <60435591+veenstrajelmer@users.noreply.github.com>
2023-10-27 19:39:28 +0200

    linting

--- xugrid/ugrid/ugrid2d.py
+++ xugrid/ugrid/ugrid2d.py
@@ -702,9 +702,7 @@ class Ugrid2d(AbstractUgrid):
         import meshkernel as mk
 
         if self._meshkernel is None:
-            self._meshkernel = mk.MeshKernel(
-                projection=mk.ProjectionType.CARTESIAN
-            )
+            self._meshkernel = mk.MeshKernel(projection=mk.ProjectionType.CARTESIAN)
             self._meshkernel.mesh2d_set(self.mesh)
         return self._meshkernel
 
