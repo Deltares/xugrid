71241a7073e61fa1b0ac02bdec7368efafda9b42
veenstrajelmer <jelmer.veenstra@deltares.nl>
2023-12-04 16:02:01 +0100

    formatting

--- tests/test_ugrid_dataset.py
+++ tests/test_ugrid_dataset.py
@@ -318,13 +318,12 @@ class TestUgridDataArray:
     def test_is_geographic(self):
         uda = self.uda
         assert uda.grid.is_geographic == False
-        
+
         uda.ugrid.set_crs(epsg=4326)
         assert uda.grid.is_geographic == True
 
         result = uda.ugrid.to_crs(epsg=28992)
         assert result.grid.is_geographic == False
-
 
     def test_to_geodataframe(self):
         with pytest.raises(ValueError, match="unable to convert unnamed"):
