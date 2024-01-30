21bd497e573b124e4b3db947029bb08952c0fe3d
Joeri van Engelen <joerivanengelen@hotmail.com>
2024-01-30 18:15:52 +0100

    Test if unique_grids function works

--- tests/test_core_utils.py
+++ tests/test_core_utils.py
@@ -8,8 +8,30 @@ Xarray is licensed under Apache License 2.0:
 https://github.com/pydata/xarray/blob/main/LICENSE
 """
 import pytest
+import numpy as np
 
-from xugrid.core.utils import either_dict_or_kwargs
+from xugrid.core.utils import either_dict_or_kwargs, unique_grids
+import xugrid
+
+def grid1d(dataset=None, indexes=None, crs=None, attrs=None):
+    xy = np.array(
+        [
+            [0.0, 0.0],
+            [1.0, 1.0],
+            [2.0, 2.0],
+        ]
+    )
+    grid = xugrid.Ugrid1d(
+        node_x=xy[:, 0],
+        node_y=xy[:, 1],
+        fill_value=-1,
+        edge_node_connectivity=np.array([[0, 1], [1, 2]]),
+        dataset=dataset,
+        indexes=indexes,
+        crs=crs,
+        attrs=attrs,
+    )
+    return grid
 
 
 def test_either_dict_or_kwargs():
@@ -23,3 +45,15 @@ def test_either_dict_or_kwargs():
 
     with pytest.raises(ValueError, match=r"foo"):
         result = either_dict_or_kwargs({"a": 1}, {"a": 1}, "foo")
+
+
+def test_unique_grids():
+    grid = grid1d()
+    grid2 = grid1d()
+    grid_different = grid1d()
+
+    grid_different.attrs["something"] = "different"
+
+    assert len(unique_grids([grid, grid2, grid_different])) == 2
+    assert len(unique_grids([grid, grid2])) == 1
+    assert len(unique_grids([grid, grid_different])) == 2
