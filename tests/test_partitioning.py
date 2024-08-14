d52c49095ea8be8f6bfcab3b757a4d554a95daf9
veenstrajelmer <jelmer.veenstra@deltares.nl>
2024-08-14 11:17:20 +0200

    fixed typo

--- tests/test_partitioning.py
+++ tests/test_partitioning.py
@@ -40,7 +40,7 @@ def test_labels_to_indices():
 
 
 def test_merge_partitions_empty_list():
-    pytest.raises(ValueError, message="Received empty partitions list, cannot be merged."):
+    with pytest.raises(ValueError, message="Received empty partitions list, cannot be merged."):
         xu.merge_partitions([])
 
 
