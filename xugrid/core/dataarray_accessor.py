9a7ff8c3949f4a828062720697e46a542a08e94a
veenstrajelmer <60435591+veenstrajelmer@users.noreply.github.com>
2024-08-20 14:05:20 +0200

    revert to_geodataframe()

--- xugrid/core/dataarray_accessor.py
+++ xugrid/core/dataarray_accessor.py
@@ -422,7 +422,7 @@ class UgridDataArrayAccessor(AbstractUgridAccessor):
         else:
             ds = self.obj.to_dataset()
 
-        variables = [varname for varname, var in ds.variables.items() if dim in var.dims]
+        variables = [var for var in ds.data_vars if dim in ds[var].dims]
         # TODO deal with time-dependent data, etc.
         # Basically requires checking which variables are static, which aren't.
         # For non-static, requires repeating all geometries.
