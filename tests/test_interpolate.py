Enable broadcasting/lazy evaluation in interpolate_na and laplace_interpolate. Fixes #292
Also changes dims into a set, consistent with xarray (future) behavior.

tests/test_interpolate.py
@@ -37,17 +37,17 @@ def test_laplace_interpolate():
     data = np.array([1.0, np.nan, np.nan, np.nan, 5.0])
     with pytest.raises(ValueError, match="connectivity is not a square matrix"):
         con = sparse.coo_matrix(coo_content, shape=(4, 5)).tocsr()
-        interpolate.laplace_interpolate(con, data, use_weights=False)
+        interpolate.laplace_interpolate(data, con, use_weights=False)
 
     expected = np.arange(1.0, 6.0)
     con = sparse.coo_matrix(coo_content, shape=(5, 5)).tocsr()
     actual = interpolate.laplace_interpolate(
-        con, data, use_weights=False, direct_solve=True
+        data, con, use_weights=False, direct_solve=True
     )
     assert np.allclose(actual, expected)
 
     actual = interpolate.laplace_interpolate(
-        con, data, use_weights=False, direct_solve=False
+        data, con, use_weights=False, direct_solve=False
     )
     assert np.allclose(actual, expected)
 
@@ -57,13 +57,11 @@ def test_nearest_interpolate():
     y = np.zeros_like(x)
     coordinates = np.column_stack((x, y))
     data = np.array([0.0, np.nan, np.nan, np.nan, 4.0])
-    actual = interpolate.nearest_interpolate(coordinates, data, np.inf)
+    actual = interpolate.nearest_interpolate(data, coordinates, np.inf)
     assert np.allclose(actual, np.array([0.0, 0.0, 0.0, 4.0, 4.0]))
 
-    actual = interpolate.nearest_interpolate(coordinates, data, 1.1)
+    actual = interpolate.nearest_interpolate(data, coordinates, 1.1)
     assert np.allclose(actual, np.array([0.0, 0.0, np.nan, 4.0, 4.0]), equal_nan=True)
 
     with pytest.raises(ValueError, match="All values are NA."):
-        interpolate.nearest_interpolate(
-            coordinates, data=np.full_like(data, np.nan), max_distance=np.inf
-        )
+        interpolate.nearest_interpolate(np.full_like(data, np.nan), coordinates, np.inf)

tests/test_ugrid2d.py
@@ -264,6 +264,22 @@ def test_to_dataset():
     check_attrs(ds)
 
 
+def test_find_ugrid_dim():
+    grid = grid2d()
+    da = xr.DataArray(data=np.ones((grid.n_face,)), dims=[grid.face_dimension])
+    assert grid.find_ugrid_dim(da) == grid.face_dimension
+
+    weird = xr.DataArray(
+        data=np.ones((grid.n_face, grid.n_node)),
+        dims=[grid.face_dimension, grid.node_dimension],
+    )
+    with pytest.raises(
+        ValueError,
+        match="UgridDataArray should contain exactly one of the UGRID dimension",
+    ):
+        grid.find_ugrid_dim(weird)
+
+
 def test_ugrid2d_set_node_coords():
     grid = grid2d()
     ds = xr.Dataset()
@@ -449,11 +465,11 @@ def test_dimensions():
     assert grid.node_dimension == f"{NAME}_nNodes"
     assert grid.edge_dimension == f"{NAME}_nEdges"
     assert grid.face_dimension == f"{NAME}_nFaces"
-    assert grid.dims == (
+    assert grid.dims == {
         f"{NAME}_nNodes",
         f"{NAME}_nEdges",
         f"{NAME}_nFaces",
-    )
+    }
     assert grid.sizes == {
         f"{NAME}_nNodes": 7,
         f"{NAME}_nEdges": 10,

xugrid/ugrid/interpolate.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
 
 import warnings
-from typing import NamedTuple, Tuple
+from typing import Any, Callable, Dict, NamedTuple, Tuple
 
 import numba as nb
 import numpy as np
+import xarray as xr
 from scipy import sparse
 from scipy.spatial import KDTree
 
@@ -197,8 +198,8 @@ class ILU0Preconditioner(NamedTuple):
 
 
 def laplace_interpolate(
-    connectivity: sparse.csr_matrix,
     data: FloatArray,
+    connectivity: sparse.csr_matrix,
     use_weights: bool,
     direct_solve: bool = False,
     delta=0.0,
@@ -218,9 +219,9 @@ def laplace_interpolate(
 
     Parameters
     ----------
+    data: ndarray of floats with shape ``(n,)``
     connectivity: scipy.sparse.csr_matrix with shape ``(n, n)``
         Sparse connectivity matrix containing ``n_nonzero`` indices and weight values.
-    data: ndarray of floats with shape ``(n,)``
     use_weights: bool, default False.
         Wether to use the data attribute of the connectivity matrix as
         coefficients. If ``False``, defaults to uniform coefficients of 1.
@@ -310,8 +311,8 @@ def laplace_interpolate(
 
 
 def nearest_interpolate(
-    coordinates: FloatArray,
     data: FloatArray,
+    coordinates: FloatArray,
     max_distance: float,
 ) -> FloatArray:
     isnull = np.isnan(data)
@@ -337,3 +338,24 @@ def nearest_interpolate(
     out = data.copy()
     out[i_target] = data[i_source[index]]
     return out
+
+
+def interpolate_na_helper(
+    da: xr.DataArray,
+    ugrid_dim: str,
+    func: Callable,
+    kwargs: Dict[str, Any],
+):
+    """Use apply ufunc to broadcast over the non UGRID dims."""
+    da_filled = xr.apply_ufunc(
+        func,
+        da,
+        input_core_dims=[[ugrid_dim]],
+        output_core_dims=[[ugrid_dim]],
+        vectorize=True,
+        kwargs=kwargs,
+        dask="parallelized",
+        keep_attrs=True,
+        output_dtypes=[da.dtype],
+    )
+    return da_filled