f2ef7526e74a197a561723ddff7c1eb85aad9a28
JoerivanEngelen <joerivanengelen@hotmail.com>
2025-04-01 18:38:23 +0200

    Add transient case

--- tests/test_regrid/test_network_gridder.py
+++ tests/test_regrid/test_network_gridder.py
@@ -46,6 +46,25 @@ def network():
     return xu.UgridDataArray(data, grid=ugrid1d)
 
 
+@pytest.fixture(scope="function")
+def points_to_sample():
+    x_loc = np.array([0.5, 1.5, 2.5, 3.5, 3.5])
+    y_loc = np.array([0.5, 1.5, 1.5, 2.5, 0.5])
+    diag = 0.5 * np.sqrt(2)
+    expected_values = np.array(
+        [
+            1.0,
+            (diag * 1 + 0.5 * 2) / (diag + 0.5),  # 1 diagonal edge, 1 horizontal edge
+            (0.5 * 2 + diag * -4 + diag * 4)
+            / (2 * diag + 0.5),  # 2 diagonal edge, 1 horizontal edge
+            -4.0,
+            4.0,
+        ]
+    )
+
+    return x_loc, y_loc, expected_values
+
+
 def test_network_gridder_init__unstructured(network, unstructured_grid):
     gridder = xu.NetworkGridder(network, unstructured_grid, method="mean")
 
@@ -57,7 +76,9 @@ def test_network_gridder_init__unstructured(network, unstructured_grid):
     assert gridder._weights.nnz == 8
 
 
-def test_network_gridder_regrid__unstructured(network, unstructured_grid):
+def test_network_gridder_regrid__unstructured(
+    network, unstructured_grid, points_to_sample
+):
     gridder = xu.NetworkGridder(network, unstructured_grid, method="mean")
     gridded = gridder.regrid(network)
 
@@ -65,18 +86,34 @@ def test_network_gridder_regrid__unstructured(network, unstructured_grid):
     assert gridded.shape == unstructured_grid.shape
     assert np.count_nonzero(np.isnan(gridded)) == 11
 
-    x_loc = [0.5, 1.5, 2.5, 3.5, 3.5]
-    y_loc = [0.5, 1.5, 1.5, 2.5, 0.5]
-    diag = 0.5 * np.sqrt(2)
+    x_loc, y_loc, expected_values = points_to_sample
     grid_values = gridded.ugrid.sel_points(x=x_loc, y=y_loc)
 
-    np.testing.assert_allclose(grid_values[0], 1.0)
-    np.testing.assert_allclose(grid_values[1], (diag * 1 + 0.5 * 2) / (diag + 0.5))
-    np.testing.assert_allclose(
-        grid_values[2], (0.5 * 2 + diag * -4 + diag * 4) / (2 * diag + 0.5)
+    np.testing.assert_allclose(grid_values, expected_values)
+
+
+def test_network_gridder_regrid__unstructured_transient(
+    network, unstructured_grid, points_to_sample
+):
+    # Make transient network
+    times = [np.datetime64("2022-01-01"), np.datetime64("2023-01-01")]
+    time_multiplier = xr.DataArray([1.0, 2.0], dims="time", coords={"time": times})
+    network = (network * time_multiplier).transpose(
+        "time", network.ugrid.grid.core_dimension
     )
-    np.testing.assert_allclose(grid_values[3], -4.0)
-    np.testing.assert_allclose(grid_values[4], 4.0)
+
+    gridder = xu.NetworkGridder(network, unstructured_grid, method="mean")
+    gridded = gridder.regrid(network)
+
+    assert isinstance(gridded, type(unstructured_grid))
+    assert np.count_nonzero(np.isnan(gridded)) == 22
+
+    x_loc, y_loc, expected_values = points_to_sample
+    grid_values_t0 = gridded.isel(time=0).ugrid.sel_points(x=x_loc, y=y_loc)
+    grid_values_t1 = gridded.isel(time=1).ugrid.sel_points(x=x_loc, y=y_loc)
+
+    np.testing.assert_allclose(grid_values_t0, expected_values)
+    np.testing.assert_allclose(grid_values_t1, 2 * expected_values)
 
 
 def test_network_gridder_init__structured(network, structured_grid):
